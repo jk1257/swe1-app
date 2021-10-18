587c173e6dede449188f3205c22867c39801ce3a
Jaspreet Kaur <jk7215@nyu.edu>
2021-10-18 00:44:31 -0400

    update

--- polls/views.py
+++ polls/views.py
@@ -13,7 +13,8 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-        Return the last five published questions (not including those set to be published in the future).
+        Return the last five published questions 
+        (not including those set to be published in the future).
         """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
